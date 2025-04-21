1b7e328bb1fb222ecae1e2582b6aca8207a47431
bobosii <day.emir@hotmail.com>
2025-04-21 18:21:05 +0300

    Initial commit

---
+++ src/main/java/com/HRManagementInformation/HrManagementInformationApplication.java
@@ -0,0 +1,13 @@
+package com.HRManagementInformation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrManagementInformationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrManagementInformationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/HRManagementInformation/HrManagementInformationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.HRManagementInformation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrManagementInformationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
